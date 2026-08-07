d79928189ec46f42e600b814b1bfad8189db331e
ANISHA KUMARI <150132638+Sriwastav07@users.noreply.github.com>
2026-08-07 23:04:42 +0530

    Time: 52 ms (79.16%), Space: 136.1 MB (61.08%) - LeetHub

---
+++ 0460-lfu-cache/0460-lfu-cache.java
@@ -0,0 +1,143 @@
+class LFUCache {
+
+    class Node {
+        int key;
+        int value;
+        int freq;
+
+        Node prev;
+        Node next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+            this.freq = 1;
+        }
+    }
+
+    class DoublyLinkedList {
+
+        Node head;
+        Node tail;
+        int size;
+
+        DoublyLinkedList() {
+            head = new Node(0, 0);
+            tail = new Node(0, 0);
+
+            head.next = tail;
+            tail.prev = head;
+            size = 0;
+        }
+
+        void add(Node node) {
+            node.next = head.next;
+            node.prev = head;
+
+            head.next.prev = node;
+            head.next = node;
+
+            size++;
+        }
+
+        void remove(Node node) {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+            size--;
+        }
+
+        Node removeLast() {
+            if (size == 0)
+                return null;
+
+            Node node = tail.prev;
+            remove(node);
+            return node;
+        }
+    }
+
+    private int capacity;
+    private int minFreq;
+
+    private HashMap<Integer, Node> cache;
+    private HashMap<Integer, DoublyLinkedList> freqMap;
+
+    public LFUCache(int capacity) {
+        this.capacity = capacity;
+        this.minFreq = 0;
+
+        cache = new HashMap<>();
+        freqMap = new HashMap<>();
+    }
+
+    public int get(int key) {
+
+        if (!cache.containsKey(key))
+            return -1;
+
+        Node node = cache.get(key);
+        updateFreq(node);
+
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+
+        if (capacity == 0)
+            return;
+
+        if (cache.containsKey(key)) {
+
+            Node node = cache.get(key);
+            node.value = value;
+            updateFreq(node);
+            return;
+        }
+
+        if (cache.size() == capacity) {
+
+            DoublyLinkedList list = freqMap.get(minFreq);
+            Node nodeToRemove = list.removeLast();
+
+            cache.remove(nodeToRemove.key);
+        }
+
+        Node newNode = new Node(key, value);
+
+        minFreq = 1;
+
+        DoublyLinkedList list = freqMap.getOrDefault(1, new DoublyLinkedList());
+        list.add(newNode);
+
+        freqMap.put(1, list);
+
+        cache.put(key, newNode);
+    }
+
+    private void updateFreq(Node node) {
+
+        int freq = node.freq;
+
+        DoublyLinkedList oldList = freqMap.get(freq);
+
+        oldList.remove(node);
+
+        if (freq == minFreq && oldList.size == 0)
+            minFreq++;
+
+        node.freq++;
+
+        DoublyLinkedList newList = freqMap.getOrDefault(node.freq, new DoublyLinkedList());
+
+        newList.add(node);
+
+        freqMap.put(node.freq, newList);
+    }
+}
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
